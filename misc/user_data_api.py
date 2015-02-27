1ef813583ed53fa35d475e3e70fe98230f6fc40c
sanjeevi rangan <sanjeevirangan@gmail.com>
2015-02-27 20:13:54 +0530

    Added support to PUT, DELETE methods

--- misc/user_data_api.py
+++ misc/user_data_api.py
@@ -1,72 +1,214 @@
 #!/Python27/python
 
+
 # imports
 import os
 import cgi, cgitb
 import MySQLdb
 import json
 from hashlib import md5
+from StringIO import StringIO
 
 
 def main():
-    print "Content-Type: text/html\n"
+    print "Content-Type: text/html\n\n"
 
-    if os.environ['REQUEST_METHOD'] == "GET":
+    if os.environ['REQUEST_METHOD'].upper() == "GET":
         execute_get()
-    elif os.environ['REQUEST_METHOD'] == "POST":
+    elif os.environ['REQUEST_METHOD'].upper() == "POST":
         execute_post()
+    elif os.environ['REQUEST_METHOD'].upper() == "PUT":
+        execute_put()
+    elif os.environ['REQUEST_METHOD'].upper() == "DELETE":
+        execute_delete()
+    else:
+        print json.dumps({'Error': "Method '{0}' not supported".format(os.environ['REQUEST_METHOD'].upper())})
 
 
 def execute_post():
     cgitb.enable()
     query = cgi.FieldStorage()
-    print type(query)
-    print query
-    #print json.dumps({'query': str(query)})
+    response = {}
     if "action" in query and "username" in query and "password" in query:
         if query["action"].value == "signup":
-            id = query["username"].value
-            #repeat_pwd = query["repeat_password"].value
-            if len(id) > 5: # and pwd == repeat_pwd:
-                first_name = query["firstname"].value
-                last_name = query["lastname"].value
-                pwd = query["password"].value
-                email = query["email_id"].value
-                encrypted_pwd = md5(pwd).hexdigest()
-                status = int(query["status"].value)
-                address = query["address"].value
-                phone = query["phone"].value
+            user_name = query["username"].value
+            first_name = query["firstname"].value
+            last_name = query["lastname"].value
+            pwd = query["password"].value
+            email = query["email_id"].value
+            encrypted_pwd = md5(pwd).hexdigest()
+            status = int(query["status"].value)
+            address = query["address"].value
+            phone = query["phone"].value
 
-                db = MySQLdb.connect("localhost", "root", "admin", "login_test", charset='utf8')
-                cursor = db.cursor()
-                sql_query = """
-                    insert into users
-                    (username, firstname, lastname, emailid, passwordhash, status, address, phone)
-                    values (%s,%s,%s,%s,%s, %s, %s, %s)
-                    on duplicate key update
-                    firstname = values(firstname),
-                    lastname = values(lastname),
-                    emailid = values(emailid),
-                    passwordhash = values(passwordhash),
-                    status = values(status),
-                    address = values(address),
-                    phone = values(phone)"""
-                sql_val_args = (id, first_name, last_name, email, encrypted_pwd, status, address, phone)
-                try:
-                    cursor.execute(sql_query, sql_val_args)
-                    db.commit()
-                    # sql_result = cursor.fetchall()
-                finally:
-                    db.close()
-                print json.dumps({'Result': 'Update success'})
+            # Create database connection
+            db = MySQLdb.connect("localhost", "root", "admin", "login_test", charset='utf8')
+            cursor = db.cursor()
+
+            # Create the sql query to create the new user. If exists, update the user data.
+            sql_query = """
+                        insert into users
+                        (username, firstname, lastname, emailid, passwordhash, status, address, phone)
+                        values (%s,%s,%s,%s,%s, %s, %s, %s)
+                        on duplicate key update
+                        firstname = values(firstname),
+                        lastname = values(lastname),
+                        emailid = values(emailid),
+                        passwordhash = values(passwordhash),
+                        status = values(status),
+                        address = values(address),
+                        phone = values(phone);
+                        """
+            sql_val_args = (user_name, first_name, last_name, email, encrypted_pwd, status, address, phone)
+            try:
+                cursor.execute(sql_query, sql_val_args)
+                db.commit()
+                # sql_result = cursor.fetchall()
+            except Exception, ex:
+                response['Error'] = "Database exception:{0}".format(ex.message)
             else:
-                print json.dumps({'Error': 'Invalid user name or password mismatch'})
+                response['Result'] = 'Update success.'
+            finally:
+                db.close()
+        else:
+            response['Error'] = "Invalid value for key 'action'."
     else:
-        print json.dumps({'Error': 'No form data'})
+        response['Error'] = 'No form data.'
+    print json.dumps(response)
 
 
 def execute_get():
-    print json.dumps({'Result': 'Nothing to get'})
+    cgitb.enable()
+    query = cgi.FieldStorage()
+    response = {}
+    result = {}
+    sql_result = ""
+    if "action" in query and "username" in query:
+        if query["action"].value == "fetch":
+            user_name = query["username"].value
+            # Create database connection
+            db = MySQLdb.connect("localhost", "root", "admin", "login_test", charset='utf8')
+            cursor = db.cursor()
+            # Create the sql query to create the new user. If exists, update the user data.
+            sql_query = """
+                        select * from users
+                        where `username` = %s ;
+                        """
+            sql_val_args = (user_name,)
+            try:
+                cursor.execute(sql_query, sql_val_args)
+                sql_response = cursor.fetchall()
+                if len(sql_response) >= 1:
+                    sql_result = sql_response[0]
+                    result['username'] = sql_result[0]
+                    result['firstname'] = sql_result[1]
+                    result['lastname'] = sql_result[2]
+                    result['emailid'] = sql_result[3]
+                    result['passwordhash'] = sql_result[4]
+                    result['address'] = sql_result[5]
+                    result['phone'] = sql_result[6]
+                    result['status'] = sql_result[7]
+                else:
+                    result ['Message'] = "No records found for user: '{0}'".format(user_name)
+                #print sql_result
+            except Exception, ex:
+                response['Error'] = "Database exception:{0}".format(ex.message)
+            else:
+                response['Result'] = result
+            finally:
+                db.close()
+        else:
+            response['Error'] = "Invalid value for key 'action'."
+    else:
+        response['Error'] = 'No form data.'
+    print json.dumps(response)
+
+
+def execute_put():
+    cgitb.enable()
+    query = cgi.FieldStorage()
+    response = {}
+    if "action" in query and query["action"].value == "upload":
+        if "userdata-file" in query:
+            file_handle = query['userdata-file']
+            print file_handle
+            file_object = file_handle.file
+            tmp_file_object = StringIO(file_object)
+            response['File-Received'] = 'Success.'
+            print
+            file_content = []
+            file_name = os.path.basename(file_handle.filename)
+            for line in file_object:
+                file_content.append(line)
+            response['File-content'] = file_content
+            with open('resource/' + file_name, 'wb') as tmp_file:
+                tmp_file.write(tmp_file_object.read())
+            response['Result'] = 'Upload success.'
+        else:
+            response['Error'] = "No File to upload."
+    else:
+        response['Error'] = "Invalid value for key 'action'."
+    print json.dumps(response)
+
+
+def execute_delete():
+    cgitb.enable()
+    query = cgi.FieldStorage()
+    response = {}
+    result = {}
+    sql_result = ""
+    if "action" in query and "username" in query:
+        if query["action"].value == "delete":
+            user_name = query["username"].value
+            # Create database connection
+            db = MySQLdb.connect("localhost", "root", "admin", "login_test", charset='utf8')
+            cursor = db.cursor()
+            # Create the sql query to create the new user. If exists, update the user data.
+
+            try:
+                sql_query = """
+                        select * from users
+                        where `username` = %s ;
+                        """
+                sql_val_args = (user_name,)
+                cursor.execute(sql_query, sql_val_args)
+                sql_response = cursor.fetchall()
+                print sql_response
+                print user_name
+                if len(sql_response) >= 1:
+                    sql_query = """
+                            delete from users
+                            where username = %s;
+                            """
+                    sql_val_args = (user_name,)
+                    cursor.execute(sql_query, sql_val_args)
+                    #sql_response = cursor.fetchall() # This gives no meaningful info
+                    db.commit()
+
+                    sql_query = """
+                        select * from users
+                        where `username` = %s ;
+                        """
+                    sql_val_args = (user_name,)
+                    cursor.execute(sql_query, sql_val_args)
+                    sql_response = cursor.fetchall()
+                    if len(sql_response) >= 1:
+                        result['Failure'] = "Record NOT deleted for user: {0}".format(user_name)
+                    else:
+                        result['Success'] = "Record deleted for user: {0}".format(user_name)
+                else:
+                    result['Error'] = "No records found for user: {0}".format(user_name)
+            except Exception, ex:
+                response['Error'] = "Database exception:{0}".format(ex.message)
+            else:
+                response['Result'] = result
+            finally:
+                db.close()
+        else:
+            response['Error'] = "Invalid value for key 'action'."
+    else:
+        response['Error'] = 'No form data.'
+    print json.dumps(response)
 
 
 main()
--- misc/user_data_client.py
+++ misc/user_data_client.py
@@ -1,32 +1,86 @@
 import argparse
 import requests
 import json
+import sys
 
 
 def main():
+    payload = {}
     parser = argparse.ArgumentParser()
     #parser.add_argument("--url", "-u", required=True)
     parser.add_argument("--action", "-a", required=True, default='signup')
-    parser.add_argument("--username", "-un", required=True)
-    parser.add_argument("--password", "-pwd", required=True)
+    parser.add_argument("--username", "-un")
+    parser.add_argument("--password", "-pwd")
     parser.add_argument("--firstname", "-fn")
     parser.add_argument("--lastname", "-ln")
     parser.add_argument("--emailid", "-e")
     parser.add_argument("--status", "-s")
     parser.add_argument("--address", "-ad")
     parser.add_argument("--phone", "-p")
+    parser.add_argument("--file", "-f")
     args = parser.parse_args()
 
-    payload = dict(action=args.action, username=args.username, password=args.password, firstname=args.firstname,
-                   lastname=args.lastname, email_id=args.emailid, status=args.status, address=args.address,
-                   phone=args.phone)
+    if args.action is not None:
+        payload['action'] = args.action
+    if args.username is not None:
+        payload['username'] = args.username.strip()
+    if args.password is not None:
+        payload['password'] = args.password
+    if args.firstname is not None:
+        payload['firstname'] = args.firstname
+    if args.lastname is not None:
+        payload['lastname'] = args.lastname
+    if args.emailid is not  None:
+        payload['email_id'] = args.emailid
+    if args.status is not None:
+        payload['status'] = args.status
+    if args.address is not None:
+        payload['address'] = args.address
+    if args.phone is not None:
+        payload['phone'] = args.phone
+    if args.file is not None:
+        payload['userdata-file'] = file(args.file, 'r')
+
     #headers = {'content-type': 'application/json'}
-    headers = {'content-type': 'application/text'}
-    #url = str(args.url)
-    url = 'http://localhost/cgi-bin/api/post-user-data.py'
-    r = requests.post(url, payload, headers=headers)
+    #headers = {'content-type': 'application/text'}
+    #headers = {'content-type': 'application/form-data'}
 
-    print r.text
+    url = 'http://localhost/cgi-bin/api/user-data-api.py'
+    '''
+    The content type should be 'x-www-form-urlencoded'. If not specified, the cgi wont read the field storage correctly.
+    '''
+    headers = {'content-type': 'application/x-www-form-urlencoded'}
 
+    if payload['action'] == 'signup':
+        if 'username' in payload and 'password' in payload:
+            r = requests.post(url, data=payload, headers=headers)
+            print r.text
+        else:
+            print "\nError: Username or password not mentioned."
+            sys.exit(1)
+    elif payload['action'] == 'fetch':
+        if 'username' in payload:
+            url += "?action=fetch&username={0}".format(payload['username'])
+            print url
+            r = requests.get(url.strip("'"))
+            print r.text
+        else:
+            print "\nError: Username not mentioned."
+            sys.exit(1)
+    elif payload['action'] == 'upload':
+        #headers = {'content-type': 'application/form-data'}
+        if 'userdata-file' in payload:
+            r = requests.put(url, data=payload)
+            print r.text
+        else:
+            print "\nError: No file to upload."
+            sys.exit(1)
+    elif payload['action'] == 'delete':
+        if 'username' in payload:
+            r = requests.delete(url, data=payload)
+            print r.text
+        else:
+            print "\nError: Username not mentioned."
+            sys.exit(1)
 
 main()
