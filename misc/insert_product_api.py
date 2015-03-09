53c754637393c42dec0ad1ec1d040b48fc3e9b1b
sanjeevi rangan <sanjeevirangan@gmail.com>
2015-03-09 19:28:45 +0530

    Create insert_product_api.py
    API to insert data into product table. Initial commit.

---
+++ misc/insert_product_api.py
@@ -0,0 +1,42 @@
+#!/Python27/python
+
+
+import cgi
+import cgitb
+import mysql_calls
+import json
+import sys
+
+
+def insert_to_product_table():
+    cgitb.enable()
+    query = cgi.FieldStorage()
+    sql_calls = mysql_calls.MySqlCalls()
+    result = {}
+    print "Content-Type: text/html\n\n"
+    if "action" in query and "product_id" in query:
+        if query["action"].value == "product":
+            result['action'] = "PRODUCT"
+            product_id = list(query["product_id"].value.split(','))
+            sql_query = "insert ignore into product (id) values (%s)"
+            sql_calls.execute_query(sql_query, product_id)
+            sql_calls.commit_db()
+            sql_calls.close_db()
+    else:
+        result['Status'] = "Invalid form key, values"
+        print json.dumps(result)
+        sys.exit(1)
+
+    if sql_calls.has_errors():
+        result['Status'] = "Task Completed with errors"
+        result['Error-Occurrence'] = str(len(sql_calls.err_list))
+        result['Errors'] = str(sql_calls.err_list)
+    else:
+        result['Status'] = "Task completed successfully."
+
+    result['Task-Execution-Time'] = sql_calls.get_total_execution_time
+    result['Report'] = sql_calls.response
+    print json.dumps(result)
+
+if __name__ == '__main__':
+    insert_to_product_table()
